8b4091e0b1780b83e99c2a5cc57a2426f322524e
nikitakhatriya <nikitakhatriya@gmail.com>
2019-08-31 09:25:17 +0530

    f

--- person/views.py
+++ person/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
 	if not request.user.is_authenticated:
-		return HttpResponse('<a href="/login">Login Here</a>')
+		return render(request, 'person/index.html')
 	else:
 		context = {
 			'UserProfile':UserProfile.objects.filter(user_name=request.user),
